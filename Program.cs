f22266ab6e57fba1c06701d4ceaa1b90851b1705
flashdens <97975794+flashdens@users.noreply.github.com>
2022-06-06 00:18:27 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Midas
+{
+    class Sentino
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tutaj bedzie cos wielkiego!");
+        }
+    }
+}
